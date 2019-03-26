c649d86779c21f8cc7701f5bf6074f4cf2d3d123
Chezzl <47607579+Chezzl@users.noreply.github.com>
2019-03-26 19:24:30 +0200

    Add files via upload

---
+++ Homework10/index.js
@@ -0,0 +1,16 @@
+50 * (42/100);
+
+80 *(400/100);
+120 * (400/100);
+
+console.log('Калорийность обеда');
+console.log(80 * (400 / 100));
+console.log('Калорийность ужина');
+console.log(120 * (400 / 100));
+
+console.log([50, 320, 480]);
+console.log({meal:'Завтрак',energy:50});
+
+var milkInGrams
+console.log(milkInGrams);
+10 
